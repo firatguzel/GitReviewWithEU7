3e22cde23740a79110103da0a037593d6bb00fdc
EU7Git&Github <firatgzl95@gmail.com>
2021-12-19 22:16:45 +0300

    Second Day of my project

---
+++ src/test/java/subject01/Test02.java
@@ -0,0 +1,12 @@
+package subject01;
+
+public class Test02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("We have made some changes at our project");
+
+
+
+    }
+}
